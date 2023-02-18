f13b7516bd868648eee136886bc28607e549bfbc
UdemeEyo <udemeeyo2013@gmail.com>
2023-02-17 17:27:04 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" -
-						Dora Korpar, 2015-10-19\n", stdout);
+	char var[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, var, sizeof(var) - 1);
+
 	return (1);
 }
