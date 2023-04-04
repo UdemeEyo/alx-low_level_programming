8dd0f3aa34c70a9bb13482ba3dd7b9085c6208a9
UdemeEyo <udemeeyo2013@gmail.com>
2023-04-04 00:16:39 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,6 @@
 /**
   * listint_len - returns the number of elements in a linked lists
   * @h: linked list of type listint_t to be returned
-  * 
   * Return: number of nodes
   */
 size_t listint_len(const listint_t *h)
