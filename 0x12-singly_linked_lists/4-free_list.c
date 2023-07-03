8db72e610bff41d14d12a01cff47d4e4867fc55a
UdemeEyo <udemeeyo2013@gmail.com>
2023-07-03 13:27:24 -0700

    mine

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list
+ * @head: list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	else
+	{
+		free_list(head->next);
+		free(head->str);
+		free(head);
+	}
+}
