80b8e230e0c9e4396a8188f27884a63dbb6ddbbf
UdemeEyo <udemeeyo2013@gmail.com>
2023-07-03 13:41:41 -0700

    mine

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+;code written by Udeme Eyo
+;dope
+;Assembly code in c language
+
+
+	global main
+	extern printf
+
+	section .text
+main:
+	mov rdi, format
+	mov  rax, 0
+	call  printf
+	mov  rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
