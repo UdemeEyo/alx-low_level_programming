91487a07a605f4b594d1a7b804ae19483e72193f
UdemeEyo <udemeeyo2013@gmail.com>
2023-09-08 13:32:11 -0700

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://raw.github.com/UdemeEyo/alx-low_level_programming/master/0x18-dynamic_libraries/test.so
-export LD_PRELOAD=/tmp/test.so
+export LD_PRELOAD=/pwd/test.so
